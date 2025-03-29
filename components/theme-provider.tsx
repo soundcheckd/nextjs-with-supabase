7fe0f3a671b7ca6e02fae70841c6b00d614b88f1
byronfirst <37887244+byronfirst@users.noreply.github.com>
2025-03-28 19:53:44 -0600

    Fix theme provider types

--- components/theme-provider.tsx
+++ components/theme-provider.tsx
@@ -2,17 +2,12 @@
 
 import * as React from "react"
 import { ThemeProvider as NextThemesProvider } from "next-themes"
+import { type ThemeProviderProps } from "next-themes/dist/types"
 
-type Attribute = "class" | "data-theme" | "data-mode"
-
-interface ThemeProviderProps {
-  children: React.ReactNode
-  attribute?: Attribute | Attribute[]
-  defaultTheme?: string
-  enableSystem?: boolean
-  disableTransitionOnChange?: boolean
-}
-
-export function ThemeProvider({ children, ...props }: ThemeProviderProps) {
-  return <NextThemesProvider {...props}>{children}</NextThemesProvider>
+export function ThemeProvider({ children, ...props }: { children: React.ReactNode } & ThemeProviderProps) {
+  return (
+    <NextThemesProvider attribute="class" defaultTheme="system" enableSystem>
+      {children}
+    </NextThemesProvider>
+  )
 } 
